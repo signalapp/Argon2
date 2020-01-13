bf8c7619de5b13cba673723130e6e95aef35711a
Alan Evans <alan@signal.org>
2020-01-13 11:39:11 -0500

    New Android app.

---
+++ app/src/main/java/org/signal/argon2/testbench/MainActivity.java
@@ -0,0 +1,14 @@
+package org.signal.argon2.testbench;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public final class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
