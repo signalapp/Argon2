e3bed6c68eaee244a2cfdcef75e859683ae47fb7
Alan Evans <alan@signal.org>
2020-01-14 08:26:45 -0500

    Tidy build script.

--- argon2/jni/Android.mk
+++ argon2/jni/Android.mk
@@ -1,19 +1,19 @@
 LOCAL_DIR := $(call my-dir)
-JNI_DIR := $(LOCAL_DIR)/../../phc-winner-argon2
+ARGON2_DIR := $(LOCAL_DIR)/../../phc-winner-argon2
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE     := argon2
-LOCAL_C_INCLUDES := $(JNI_DIR)/include/
+LOCAL_C_INCLUDES := $(ARGON2_DIR)/include/
 LOCAL_CFLAGS     += -Wall
 
 LOCAL_SRC_FILES := $(LOCAL_DIR)/org_signal_argon2_Argon2Native.c \
-                   $(JNI_DIR)/src/blake2/blake2b.c \
-                   $(JNI_DIR)/src/argon2.c \
-                   $(JNI_DIR)/src/core.c \
-                   $(JNI_DIR)/src/encoding.c \
-                   $(JNI_DIR)/src/genkat.c \
-                   $(JNI_DIR)/src/ref.c \
-                   $(JNI_DIR)/src/thread.c
+                   $(ARGON2_DIR)/src/blake2/blake2b.c \
+                   $(ARGON2_DIR)/src/argon2.c \
+                   $(ARGON2_DIR)/src/core.c \
+                   $(ARGON2_DIR)/src/encoding.c \
+                   $(ARGON2_DIR)/src/genkat.c \
+                   $(ARGON2_DIR)/src/ref.c \
+                   $(ARGON2_DIR)/src/thread.c
 
 include $(BUILD_SHARED_LIBRARY)
